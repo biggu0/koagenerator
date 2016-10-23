73292e262643fe4dae8fbb9fbbf31915e08da2fc
guoqing <guoqing07@meituan.com>
2016-10-23 22:26:46 +0800

    go sleep

--- route/index.js
+++ route/index.js
@@ -1,20 +1,8 @@
 var router = require('koa-router')();
 var fetch = require('node-fetch');
 var co = require('co');
-var movieData = require("../model/movie.js");
+var  = require("../model/movie.js");
 
-router.get('/', function *(){
-	// Get ../model movie data
-	var data = yield new movieData(),
-		movie = data.movie;
-
-    yield this.render('index', {
-    	title : 'koa is coming',
-    	list : movie,
-    	version : data.$API_VERSION
-    });
-    // yield this.render('page/index', {title:'koa is coming'});
-});
 
 router.get('/index', function *(){
     yield this.render('page/index', {title:'koa is coming'});
